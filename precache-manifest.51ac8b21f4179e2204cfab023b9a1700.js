cc8aa6863abb31e620ebe44f415dc513c7a7f7f3
mac <mac@Raymonds-MacBook-Pro.local>
2020-05-02 19:40:45 +0100

    Updates

---
+++ precache-manifest.51ac8b21f4179e2204cfab023b9a1700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eccc34c7d7b1cf9bfed09cd3e8ae96c",
+    "url": "/News-App/index.html"
+  },
+  {
+    "revision": "bf3882ff67c59cb04f56",
+    "url": "/News-App/static/css/main.49fa7901.chunk.css"
+  },
+  {
+    "revision": "428091ab8840fc2b8c43",
+    "url": "/News-App/static/js/2.540770c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/News-App/static/js/2.540770c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3882ff67c59cb04f56",
+    "url": "/News-App/static/js/main.aae5a28a.chunk.js"
+  },
+  {
+    "revision": "8b76d7df666274bd3cde",
+    "url": "/News-App/static/js/runtime-main.dbee0418.js"
+  }
+]);
